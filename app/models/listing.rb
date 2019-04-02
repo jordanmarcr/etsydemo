74aa4826f2abd853d1f1ab35af3016cec5e87da6
Jordan Rabinowitz <jordanmarcr@gmail.com>
2019-04-02 18:43:26 -0400

    missing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,8 @@ class Listing < ApplicationRecord
   has_attached_file :image,
   styles: {
     medium: "200x",
-    thumb: "100x100" }
+    thumb: "100x100" },
+    default_url: "http://identifyla.lsu.edu/peopleimages/noimage.jpg"
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
