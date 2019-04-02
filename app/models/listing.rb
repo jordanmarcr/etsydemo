b3c7680e4078394184b7718b633f2ee8a0b6d06e
Jordan Rabinowitz <jordanmarcr@gmail.com>
2019-04-02 18:42:23 -0400

    testing

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,8 +2,7 @@ class Listing < ApplicationRecord
   has_attached_file :image,
   styles: {
     medium: "200x",
-    thumb: "100x100" },
-    default_url: "missing_image.jpeg"
+    thumb: "100x100" }
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 end
