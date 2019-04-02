11e8c95456fb766051cd9bcb08f5ee196805da55
Jordan Rabinowitz <jordanmarcr@gmail.com>
2019-04-02 19:12:09 -0400

    aws gem version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY_ID'],
       :s3_region => ENV['AWS_REGION'],
-      :s3_host_name: ENV["S3_HOST_NAME"]
+      :s3_host_name => ENV["S3_HOST_NAME"]
     },
   }
 
