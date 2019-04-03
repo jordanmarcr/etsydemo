d5f891eceb18692d6fb14b558101dcb1483bb9ae
Jordan Rabinowitz <jordanmarcr@gmail.com>
2019-04-03 12:51:25 -0400

    enable users to place orders

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,10 +7,11 @@ class Listing < ApplicationRecord
 
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
-
   validates :name, :description, :price, presence: true
   validates :price, numericality: {greater_than: 0}
   validates_attachment_presence :image
 
   belongs_to :user
+  has_many :orders
 end
+
